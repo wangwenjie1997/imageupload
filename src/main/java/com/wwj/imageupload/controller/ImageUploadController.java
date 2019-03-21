d765192568273b1abecd95c8bae608cd6135eaec
wangwenjie1997 <2589856016@qq.com>
2019-03-21 21:45:24 +0800

    1.增加图片预览功能 2.添加一些功能说明

--- src/main/java/com/wwj/imageupload/controller/ImageUploadController.java
+++ src/main/java/com/wwj/imageupload/controller/ImageUploadController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,13 +30,10 @@ public class ImageUploadController {
     @RequestMapping("/imageUpload")
     public String imageUpload(@RequestParam("fileName") MultipartFile file){
         String result_msg="";//上传结果信息
-        boolean upload_result=true;//上传结果
-        //result_msg和upload_result可用一个int型参数代替(如0表示xx,1表示xx)
 
         Map<String,Object> root=new HashMap<String, Object>();
 
         if (file.getSize() / 1000 > 100){
-            upload_result=false;
             result_msg="图片大小不能超过100KB";
         }
         else{
@@ -43,26 +42,24 @@ public class ImageUploadController {
             if (fileType.equals("image/jpeg") || fileType.equals("image/png") || fileType.equals("image/jpeg")) {
                 // 要上传的目标文件存放的绝对路径
                 String localPath="F:\\IDEAProject\\imageupload\\src\\main\\resources\\static\\img";
-                //上传后保存的文件名
-                String fileName=file.getOriginalFilename();
+                //上传后保存的文件名(需要防止图片重名导致的文件覆盖)
+                String str = (new SimpleDateFormat("yyyyMMddHHmmssSSS")).format(new Date());
+                String fileName=str+file.getOriginalFilename();
                 if (FileUtils.upload(file, localPath, fileName)) {
                     //文件存放的相对路径(一般存放在数据库用于img标签的src)
                     String relativePath="img/"+file.getOriginalFilename();
-                    root.put("relativePath",relativePath);
+                    root.put("relativePath",relativePath);//前端根据是否存在该字段来判断上传是否成功
                     result_msg="图片上传成功";
                 }
                 else{
-                    upload_result=false;
                     result_msg="图片上传失败";
                 }
             }
             else{
-                upload_result=false;
                 result_msg="图片格式不正确";
             }
         }
 
-        root.put("upload_result",upload_result);
         root.put("result_msg",result_msg);
 //        JSON.toJSONString(root,SerializerFeature.DisableCircularReferenceDetect);
         String root_json=JSON.toJSONString(root);
